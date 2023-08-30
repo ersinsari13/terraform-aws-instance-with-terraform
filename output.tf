4b26ff70726e0470269c1dc1ea67e41296b53b01
ersin <sariiersinn13@gmail.com>
2023-08-30 07:58:05 +0300

    should add your key-pair

--- output.tf
+++ output.tf
@@ -2,5 +2,5 @@ output "instance_public_ip" {
   value = aws_instance.terraform_server.public_ip 
 }
 output "instance_id" {
-  value _ aws_instance.terraform_server.instance_id
+  value = aws_instance.terraform_server.instance_id
 }
